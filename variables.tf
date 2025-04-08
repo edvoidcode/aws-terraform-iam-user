4b550bacd9f47a78e8a637fac0e62e0bcede3c6e
edvoidcode <edsonsilva.contato@gmail.com>
2025-04-08 00:14:21 -0300

    corrigindo erros secrets

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
-variable "aws_access_key" {
-  description = "Chave de acesso da AWS (do usuário terraform-admin)"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_access_key" {
+#   description = "Chave de acesso da AWS (do usuário terraform-admin)"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "aws_secret_key" {
-  description = "Chave secreta da AWS (do usuário terraform-admin)"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_secret_key" {
+#   description = "Chave secreta da AWS (do usuário terraform-admin)"
+#   type        = string
+#   sensitive   = true
+# }
 
 variable "aws_region" {
   description = "Região da AWS"
