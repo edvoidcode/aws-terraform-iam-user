53b5f719cf6ab9671723740218d0716ccbe95dd9
edvoidcode <edsonsilva.contato@gmail.com>
2025-04-08 14:42:58 -0300

    mudando region

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.94.1"
     }
   }
 }
 
 provider "aws" {
-  region                  = var.aws_region
-#   access_key              = var.aws_access_key
-#   secret_key              = var.aws_secret_key
+  region = var.aws_region
+  #   access_key              = var.aws_access_key
+  #   secret_key              = var.aws_secret_key
 }
