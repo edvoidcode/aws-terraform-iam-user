f4da49666b8011ffff3d5952548f6e091379fe59
edvoidcode <edsonsilva.contato@gmail.com>
2025-04-08 11:33:45 -0300

    comentando provider

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.94.1"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.94.1"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = var.aws_region
